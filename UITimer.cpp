5fe07ca1a49799d25140cd72d62b087c9b487a68
YB970902 <sssd00123@naver.com>
2021-09-28 14:56:00 +0900

    UITimer 추가

---
+++ UITimer.cpp
@@ -0,0 +1,51 @@
+#include "UITimer.h"
+#include "Image.h"
+#include "Command.h"
+
+void UITimer::Init()
+{
+	char text[128];
+	for (int i = 0; i < 10; i++)
+	{
+		mImgMap[i] = new Image();
+		wsprintf(text, "Image/UI/%d.bmp", i);
+		mImgMap[i]->Init(text, TimeImageWidth, TimeImageHeight);
+	}
+}
+
+void UITimer::Release()
+{
+	for (int i = 0; i < 10; i++)
+	{
+		mImgMap[i]->Release();
+		mImgMap[i] = nullptr;
+	}
+}
+
+void UITimer::Update()
+{
+	mElapseTime++;
+	if (mElapseTime > 100)
+	{
+		mElapseTime = 0;
+		mCurTime--;
+		if (mCurTime < 0)
+		{
+			if (mCom) { mCom->Execute(); }
+		}
+	}
+}
+
+void UITimer::Render(HDC hdc)
+{
+	if (mCurTime < 0)
+	{
+		mImgMap[0]->Render(hdc, WIN_SIZE_X / 2 - TimeImageWidthHalf, TimeImageHeight);
+		mImgMap[0]->Render(hdc, WIN_SIZE_X / 2 + TimeImageWidthHalf, TimeImageHeight);
+	}
+	else
+	{
+		mImgMap[mCurTime / 10]->Render(hdc, WIN_SIZE_X / 2 - TimeImageWidthHalf, TimeImageHeight);
+		mImgMap[mCurTime % 10]->Render(hdc, WIN_SIZE_X / 2 + TimeImageWidthHalf, TimeImageHeight);
+	}
+}
---
+++ UITimer.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <map>
+#include "GameEntity.h"
+
+using namespace std;
+
+class Command;
+class Image;
+class UITimer : public GameEntity
+{
+protected:
+	const int TimeImageWidth = 68;
+	const int TimeImageHeight = 100;
+	const int TimeImageWidthHalf = 34;
+	const int TimeImageHeightHalf = 50;
+	map<int, Image*> mImgMap;
+	int mElapseTime = 0;
+	int mCurTime = 0;
+	Command* mCom = nullptr;
+public:
+	void Init();
+	void Release();
+	void Update();
+	void Render(HDC hdc);
+
+	inline void SetTime(int time) { mCurTime = time; }
+	inline void SetCommand(Command* com) { mCom = com; }
+};
+
